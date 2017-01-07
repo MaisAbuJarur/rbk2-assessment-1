7d4c11dacccbcf87886c62a8fab88bc5715e7a25
MaisAbuJarur <maisabujarur@gmail.com>
2017-01-07 10:06:07 +0200

    2 1

--- ds-stack/ds-stack.js
+++ ds-stack/ds-stack.js
@@ -1,3 +1,16 @@
 var Stack = function() {
 	//your code is here
+	this._storage = [];
+
+
+Stack.prototype.add = function (value){
+	this._storage.push(value)
+}
+
+Stack.prototype.remove = function () {
+	var value = this._storage[this._storage.length-1]
+	this._storage.pop()
+	return value;
+}
+
 };
